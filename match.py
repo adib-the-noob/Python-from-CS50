3458305f3332864998424ca9b6fd8811439bbdba
Mohammed Adib <mdadib550@gmail.com>
2023-02-01 14:09:21 +0600

    Match Added

---
+++ match.py
@@ -0,0 +1,11 @@
+name = input("your name: ")
+
+match name:
+    case "Adib":
+        print("Hello Mr. Noob")
+    case "Alex":
+        print("Hello Mr. Pythonista")
+    case "Marzan":
+        print("Hello Mr. Marzan")   
+    case _ :
+        print("Who are you?")
