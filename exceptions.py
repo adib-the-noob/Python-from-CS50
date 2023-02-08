9c4260b9ed035936fdaa3a41b2a26915866ae8c3
Mohammed Adib <mdadib550@gmail.com>
2023-02-08 17:18:03 +0600

    Function Arguments

--- exceptions.py
+++ exceptions.py
@@ -1,14 +1,19 @@
 def main():
-    x = get_value()
+    x = get_value("What is the value of X?")
     print(f"Value of x is {x}")
 
-def get_value():
+def get_value(prompt):
     while True:
         try:
-            name = int(input("Enter your name: "))
+            name = int(input(prompt))
         except ValueError:
             print("X is not a number")
         else:
             return name
     
 main()
+
+# try:
+#     x = int(input("Enter a number: "))
+# except ValueError:
+#     pass
